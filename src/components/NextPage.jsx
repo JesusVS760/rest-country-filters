e3305a192bdaff78b9eb4ed69d755489d5b38046
JesusVA760 <tonio4340406@gmail.com>
2024-08-20 15:46:03 -0700

    NextPage

--- src/components/NextPage.jsx
+++ src/components/NextPage.jsx
@@ -1,23 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./NextPage.css";
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const NextPage = ({ sendData }) => {
   const [matchData, setMatchData] = useState(sendData);
-  const [foundCountry, setFoundCountry] = useState({});
-  //   console.log("Countries", matchData);
+  const [foundCountry, setFoundCountry] = useState(null);
+  const [currencies, setCurrencies] = useState([]);
+  const [languages, setLanguages] = useState([]);
 
   const { countryName } = useParams(); // extracts URL NAME TO MAKE AVAIABLE TO COMPONENT
 
-  const countryFound = matchData.filter(
-    (country) => country.name === countryName
-  );
-  console.log(countryFound);
+  useEffect(() => {
+    if (countryName !== "") {
+      try {
+        const countryFound = matchData.filter(
+          (country) => country.name == countryName
+        );
+        // If a country is found, set the first match (assuming only one match is expected)
+        if (countryFound.length > 0) {
+          setFoundCountry(countryFound[0]);
+        } else {
+          setFoundCountry(null); // No match found
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [matchData, countryName]);
+
+  useEffect(() => {
+    if (foundCountry != null) {
+      setCurrencies(foundCountry.currencies[0]);
+      setLanguages(foundCountry.languages[0]);
+    }
+  }, [foundCountry]);
+  console.log("LANG", languages);
+
+  // console.log(foundCountry);
+  console.log("CURR:", currencies);
+
   return (
-    <div className="next-page-container">
+    <div>
       <Navbar />
-      {countryName};
+      {foundCountry != null ? (
+        <div className="foundCountry-container">
+          <div className="foundCountry-image">
+            <img src={foundCountry.flag} alt="found image" />
+          </div>
+          <div className="foundCountry-content">
+            <div className="col1">
+              <h1>{foundCountry.name}</h1>
+
+              <p>Native Name: {foundCountry.altSpellings[1]}</p>
+              <p>Population: {foundCountry.population}</p>
+              <p>Region: {foundCountry.region}</p>
+              <p>Sub Region {foundCountry.subregion}</p>
+              <p>Capital: {foundCountry.capital}</p>
+            </div>
+            <div className="col2">
+              <p>Top Level Domain: {foundCountry.topLevelDomain[0]}</p>
+              <p>Currencies: {currencies.name}</p>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}{" "}
     </div>
   );
 };
