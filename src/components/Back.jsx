3b15c4502fb191b367efa647016efb9289bb2583
JesusVA760 <tonio4340406@gmail.com>
2024-08-21 16:08:23 -0700

    Back Button logic & style

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import arrow from "../assets/back-arrow.png";
+import "./Back.css";
+import { useNavigate } from "react-router-dom";
 
-const Back = () => {
-  return <div className="back-container"></div>;
+const Back = ({ sendMode }) => {
+  const [backMode, setBackMode] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleBackPage = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className={!sendMode ? "back-container-dark" : "back-container"}>
+      <button
+        onClick={() => {
+          setBackMode(!sendMode);
+          handleBackPage();
+        }}
+      >
+        <img src={arrow} alt="arrow" />
+        Back
+      </button>
+    </div>
+  );
 };
 
 export default Back;
--- src/pages/NextPage.jsx
+++ src/pages/NextPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./NextPage.css";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Back from "../components/Back";
 
 const NextPage = ({ sendData }) => {
   const [matchData, setMatchData] = useState(sendData);
@@ -50,41 +51,50 @@ const NextPage = ({ sendData }) => {
 
   return (
     <div>
-      <Navbar modeColor={modeColor} />
+      <div className="navbar">
+        <Navbar modeColor={modeColor} />
+      </div>
       {foundCountry != null ? (
         <div
           className={
             matchMode ? "foundCountry-container-dark" : "foundCountry-container"
           }
         >
-          <div className="foundCountry-image">
-            <img src={foundCountry.flag} alt="found image" />
-          </div>
-          <div className="div">
-            <div className="foundCountry-content">
-              <div className="col1">
-                <h1>{foundCountry.name}</h1>
-
-                <p>Native Name: {foundCountry.altSpellings[1]}</p>
-                <p>Population: {foundCountry.population}</p>
-                <p>Region: {foundCountry.region}</p>
-                <p>Sub Region: {foundCountry.subregion}</p>
-                <p>Capital: {foundCountry.capital}</p>
-              </div>
-              <div className="col2">
-                <p>Top Level Domain: {foundCountry.topLevelDomain[0]}</p>
-                <p>Currencies: {currencies.name}</p>
-                <p>
-                  Languages:
-                  {languages.map((lang) => ` ${lang.name}`).join(", ")}
-                </p>
-                <p></p>
-              </div>
+          <div className="foundCountry-wrapper">
+            <div className="back-button">
+              <Back sendMode={matchMode} />
             </div>
-            <div className="border-countries">
-              <p>Border Countries: </p>
-              <div className="border">
-                <p>{`${borders}`} </p>
+            <div className="foundCountry-content">
+              <div className="foundCountry-image">
+                <img src={foundCountry.flag} alt="found image" />
+              </div>
+              <div className="div">
+                <div className="foundCountry-content">
+                  <div className="col1">
+                    <h1>{foundCountry.name}</h1>
+
+                    <p>Native Name: {foundCountry.altSpellings[1]}</p>
+                    <p>Population: {foundCountry.population}</p>
+                    <p>Region: {foundCountry.region}</p>
+                    <p>Sub Region: {foundCountry.subregion}</p>
+                    <p>Capital: {foundCountry.capital}</p>
+                  </div>
+                  <div className="col2">
+                    <p>Top Level Domain: {foundCountry.topLevelDomain[0]}</p>
+                    <p>Currencies: {currencies.name}</p>
+                    <p>
+                      Languages:
+                      {languages.map((lang) => ` ${lang.name}`).join(", ")}
+                    </p>
+                    <p></p>
+                  </div>
+                </div>
+                <div className="border-countries">
+                  <p>Border Countries: </p>
+                  <div className="border">
+                    <p>{`${borders}`} </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
